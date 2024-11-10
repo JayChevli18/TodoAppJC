b1215d97e12daf057b80a552b12fc23a41e09032
JayChevli18 <chevlijay70@gmail.com>
2024-11-10 22:54:05 +0530

    Testing

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,14 @@
-const express=require("express");
-const dotenv=require("dotenv");
-const connectdb=require("./config/dbConfig");
-const authRoutes=require("./routes/authRoutes");
-const todoRoutes=require("./routes/todoRoutes");
-const cors=require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectdb = require("./config/dbConfig");
+const authRoutes = require("./routes/authRoutes");
+const todoRoutes = require("./routes/todoRoutes");
+const cors = require("cors");
 dotenv.config();
 
 connectdb();
 
-const app=express();
+const app = express();
 
 app.use(cors({
     // origin: 'http://localhost:3000',
@@ -19,13 +19,25 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
-app.get('/x',(req, res)=>{
+app.get('/x', (req, res) => {
     res.json("ok");
 })
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.json("Hello");
 })
 
-module.exports=app;
+app.post('/api/user', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(name, email, password);
+        const token = email;
+        res.status(201).json({ success: true, token });
+    }
+    catch (err) {
+        res.status(400).json({ success: false, error: err.message })
+    }
+})
+
+module.exports = app;
 
 
