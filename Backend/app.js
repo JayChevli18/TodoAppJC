c51ada6aeac5c217093123ff9b07486565a1cec5
JayChevli18 <chevlijay70@gmail.com>
2024-11-10 22:11:47 +0530

    Testing 1

--- Backend/app.js
+++ Backend/app.js
@@ -19,6 +19,9 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
+app.get('/x',(req, res)=>{
+    res.json("ok");
+})
 app.get('/', (req, res)=>{
     res.json("Hello");
 })
