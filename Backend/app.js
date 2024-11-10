dc174065452bc6a73bd32051b6e353c4e4bd7c80
JayChevli18 <chevlijay70@gmail.com>
2024-11-10 21:38:50 +0530

    Testing Deploy

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,9 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
-
+app.get('/', (req, res)=>{
+    res.json("Hello");
+})
 
 module.exports=app;
 
