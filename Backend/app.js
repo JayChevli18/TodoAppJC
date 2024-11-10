7d0ef3a3ca0bc57b2bc5e7106f16a6e887e4ca71
JayChevli18 <chevlijay70@gmail.com>
2024-11-10 18:21:48 +0530

    Revert "Adding CORS"
    This reverts commit 054d09c48b2b3f587182238db76ce392265762cd.

--- Backend/app.js
+++ Backend/app.js
@@ -3,18 +3,13 @@ const dotenv=require("dotenv");
 const connectdb=require("./config/dbConfig");
 const authRoutes=require("./routes/authRoutes");
 const todoRoutes=require("./routes/todoRoutes");
-const cors=require("cors");
+
 dotenv.config();
 
 connectdb();
 
 const app=express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true
-}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
